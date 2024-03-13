183fc307adf6fe41b8a620ea0619084d2feab61a
Gustavo Lamb <gustavo.lamb01@gmail.com>
2024-03-13 15:58:11 -0300

    Ajustes na questão-b da maratona
    Nesse MR foi feit alguns ajustes na questão-b da maratona visto que
    saida estava errada

--- maratona/questao-b.py
+++ maratona/questao-b.py
@@ -3,11 +3,23 @@ def inverte_niveis_arvore(arvore_array):
         if index < 0:
             return arvore_array
 
-        index_direito = 2 * index + 2
-        index_esquerdo = 2 * index + 1
+        filho_direito_index = 2 * index + 2
+        filho_esquerdo_index = 2 * index + 1
 
-        if index_direito < len(arvore_array) and index_esquerdo < len(arvore_array):
-            arvore_array[index_direito], arvore_array[index_esquerdo] = arvore_array[index_esquerdo], arvore_array[index_direito]
+        if filho_direito_index < len(arvore_array) and filho_esquerdo_index < len(arvore_array):
+            arvore_array[filho_direito_index], arvore_array[filho_esquerdo_index] = arvore_array[filho_esquerdo_index], arvore_array[filho_direito_index]
+
+            if index % 2 != 0 or index == 0:
+                irmao_direito_index = (2 * ((index -1) // 2)) + 2
+
+                irmao_direito_filho_direito = 2 * irmao_direito_index + 2
+                irmao_direito_filho_esquerdo = 2 * irmao_direito_index + 1
+
+                valor_filho_direito = arvore_array[filho_direito_index]
+                valor_filho_esquerdo = arvore_array[filho_esquerdo_index]
+
+                arvore_array[filho_direito_index], arvore_array[filho_esquerdo_index] = arvore_array[irmao_direito_filho_direito], arvore_array[irmao_direito_filho_esquerdo]
+                arvore_array[irmao_direito_filho_direito], arvore_array[irmao_direito_filho_esquerdo] = valor_filho_direito, valor_filho_esquerdo
 
         return inverte_niveis_arvore(arvore_array, index - 1)
 
